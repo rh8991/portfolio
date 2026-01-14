690f2186428ee4742424e6f34e8bd3aa8a08d41c
rh8991 <103560124+rh8991@users.noreply.github.com>
2026-01-14 02:40:32 +0200

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { ThemeProvider } from './context/ThemeContext';
 import Home from './pages/Home';
